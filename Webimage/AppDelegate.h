5228f476eaa5f49d9050b363d57f2d5ad02755b5
Daniel <djs66256@163.com>
2017-04-19 00:23:41 +0800

    Initial Commit

---
+++ Webimage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Webimage
+//
+//  Created by daniel on 2017/4/19.
+//  Copyright © 2017年 Daniel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Webimage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Webimage
+//
+//  Created by daniel on 2017/4/19.
+//  Copyright © 2017年 Daniel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
